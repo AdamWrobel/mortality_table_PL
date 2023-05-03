d53500afd5e3c9fa8509ab67047d63b85d9a8fe0
Adam Wróbel <wrobel_adam@o2.pl>
2023-05-03 22:44:27 +0200

    modifications to improve format/naming

--- mortality_tables_into_csv.R
+++ mortality_tables_into_csv.R
@@ -6,12 +6,17 @@ path = "C:/Users/Adam/OneDrive/Projekty/Programowanie_R/tablice_trwania_zycia_19
 mortality_list <- list()
 columns <- colnames(read_excel(path, sheet = '1990', skip = 3))
 for (year in as.character(1990:2021)){
-    mortality_list[[year]] <- read_excel(path, sheet = year, skip = 3)
-    colnames(mortality_list[[year]]) <- columns}
+    single_year <- read_excel(path, sheet = year, skip = 3)  
+    colnames(single_year)[c(1,2)] <- c('sex','age')
+    
+    mortality_list[[year]] <- single_year %>% 
+        mutate(gender = case_when(sex == 1 ~ 'Male', sex == 2 ~ 'Female'),
+               year = year) %>% 
+        select(gender, age, qx, ex, year)
+    }
 
 str(mortality_list)
 
 mortality_table <- bind_rows(mortality_list)
-
 
 write.csv(mortality_table, file =  "C:/Users/Adam/OneDrive/Projekty/Programowanie_R/Polskie_tablice_trwania_zycia_1990_2021.csv", row.names = F)
